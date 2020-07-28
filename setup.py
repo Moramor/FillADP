0ab815f195a0404d17903d72a57587bc00ff49b3
Ludo <ludovic.bondon@navya.tech>
2020-07-28 17:03:16 +0200

    Make script packaged. Make it already-enterinated proof
    WIP save last filled date not working

---
+++ setup.py
@@ -0,0 +1,31 @@
+import re
+from pathlib import Path
+from setuptools import setup
+
+
+def get_dependencies():
+    requirements = Path(__file__).resolve().parent / "requirements.txt"
+    with requirements.open() as fh:
+        requirements = fh.readlines()
+        return list(
+            map(
+                lambda x: re.search("([^>=<~]*)[>=<~]", x).group(1),
+                map(str.strip, requirements),
+            )
+        )
+
+
+setup(
+    name="adprout",
+    version="1.0.0",
+    description="Automatically fill ADP day to day hours and projects with the same hours and the same project",
+    url="https://github.com/Moramor/ADPROUT",
+    entry_points={"console_scripts": ["adprout=adprout.filler:fill_adp"]},
+    author="Ludovic Bondon",
+    author_email="ludovicbondon@gmail.com",
+    python_requires=">=3.6",
+    packages=["adprout"],
+    install_requires=get_dependencies(),
+    zip_safe=False,
+    include_package_data=True,
+)
